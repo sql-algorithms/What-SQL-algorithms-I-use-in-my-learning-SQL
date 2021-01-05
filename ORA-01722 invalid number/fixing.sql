2e446d491b78da8e0a9c85152a8786fa2a6b1a03
iostream17 <17309940+iostream17@users.noreply.github.com>
2021-01-05 17:11:30 +0200

    update

---
+++ ORA-01722 invalid number/fixing.sql
@@ -0,0 +1,133 @@
+﻿SELECT 
+D.UNIT_CODE,
+         CASE 
+         
+              WHEN D.CAPITOL = 416 THEN 'CAP1'
+              WHEN D.CAPITOL = 417 THEN 'CAP2'
+              WHEN D.CAPITOL = 418 THEN 'Informativ'
+              WHEN D.CAPITOL = 419 THEN 'Cap. SR'
+         END 
+          AS CAPITOL,
+         D.RIND, 
+D.COL1 AS COL1,
+D.COL2 AS COL2, 
+D.COL3 AS COL3,  
+D.COL4 AS COL4, 
+D.COL5 AS COL5,  
+D.COL6 AS COL6,    
+D.COL7 AS COL7,
+D.COL8 AS COL8, 
+D.COL9 AS COL9, 
+D.COL10 AS COL10,  
+D.COL11 AS COL11, 
+D.COL12 AS COL12, 
+D.COL13 AS COL13,
+D.COL14 AS COL14,   
+D.COL15 AS COL15,     
+D.COL16 AS COL16,
+D.COL17 AS COL17,
+D.COL18 AS COL18,
+D.COL19 AS COL19   
+  
+FROM
+  VW_DATA_ALL_GC  D
+
+
+WHERE 
+
+--------------------------------------
+ (D.PERIOADA = 1047)  AND   
+  (D.FORM = 58) AND
+  (D.FORM_VERS =1004) AND 
+  (:pID_MDTABLE =:pID_MDTABLE) AND 
+  (D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%') AND
+  D.FORM IN (58)  AND
+  D.CAPITOL IN (419,416,417,418) AND 
+
+
+
+-----------------------------------
+REGEXP_LIKE (D.COL1, '[[:alpha:]]')
+OR 
+REGEXP_LIKE (D.COL16, '[[:alpha:]]')
+OR 
+REGEXP_LIKE (D.COL2, '[[:alpha:]]')
+OR 
+REGEXP_LIKE (D.COL3, '[[:alpha:]]')
+OR 
+REGEXP_LIKE (D.COL4, '[[:alpha:]]')
+OR 
+REGEXP_LIKE (D.COL5, '[[:alpha:]]')
+OR 
+REGEXP_LIKE (D.COL6, '[[:alpha:]]')
+
+OR 
+REGEXP_LIKE (D.COL7, '[[:alpha:]]')
+OR 
+REGEXP_LIKE (D.COL8, '[[:alpha:]]')
+
+OR 
+REGEXP_LIKE (D.COL9, '[[:alpha:]]')
+
+
+OR 
+REGEXP_LIKE (D.COL10, '[[:alpha:]]')
+
+OR 
+REGEXP_LIKE (D.COL11, '[[:alpha:]]')
+
+OR 
+REGEXP_LIKE (D.COL12, '[[:alpha:]]')
+
+OR 
+REGEXP_LIKE (D.COL13, '[[:alpha:]]')
+
+
+OR 
+REGEXP_LIKE (D.COL14, '[[:alpha:]]')
+
+OR 
+REGEXP_LIKE (D.COL15, '[[:alpha:]]')
+
+OR 
+REGEXP_LIKE (D.COL16, '[[:alpha:]]')
+
+OR 
+REGEXP_LIKE (D.COL17, '[[:alpha:]]')
+
+
+OR 
+REGEXP_LIKE (D.COL18, '[[:alpha:]]')
+
+OR 
+REGEXP_LIKE (D.COL19, '[[:alpha:]]')
+
+ GROUP BY 
+   D.UNIT_CODE,
+   D.CAPITOL,
+   D.RIND,
+   D.COL16,
+   D.COL1,
+   D.COL2,
+   D.COL3,
+   D.COL4,
+   D.COL5,
+   D.COL6,
+   D.COL7,
+   D.COL8,
+   D.COL9,
+   D.COL10,
+   D.COL11,
+   D.COL12,
+   D.COL13,
+   D.COL14,
+   D.COL15,
+   D.COL16,
+   D.COL17,
+   D.COL18,
+   D.COL19      
+            
+  
+    ORDER BY 
+   D.CAPITOL,
+   D.RIND
