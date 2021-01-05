2106a5ae5a020e4035fa3faf6ab7977b27b3a35c
iostream17 <17309940+iostream17@users.noreply.github.com>
2021-01-05 15:01:50 +0200

    update

---
+++ ORA-01722 invalid number/dupa_cap_rind_.2_CMPAsql_ver_1.sql
@@ -0,0 +1,68 @@
+﻿SELECT 
+         D.UNIT_CODE,
+       CASE 
+         
+              WHEN D.CAPITOL = 416 THEN 'CAP1'
+              WHEN D.CAPITOL = 417 THEN 'CAP2'
+              WHEN D.CAPITOL = 418 THEN 'Informativ'
+              WHEN D.CAPITOL = 419 THEN 'Cap. SR'
+         END 
+          AS CAPITOL,
+         D.RIND,
+        TO_NUMBER(NVAL(MAX(D.COL1))) AS COL1,
+        TO_NUMBER(NVAL(MAX(D.COL2))) AS COL2,
+         TO_NUMBER(NVAL(MAX(D.COL3))) AS COL3,
+        TO_NUMBER(NVAL(MAX(D.COL4))) AS COL4,
+        TO_NUMBER(NVAL(MAX(D.COL5))) AS COL5,
+         TO_NUMBER(NVAL(MAX(D.COL6))) AS COL6,
+         TO_NUMBER(NVAL(MAX(D.COL7))) AS COL7,
+         TO_NUMBER(NVAL(MAX(D.COL8))) AS COL8,
+         TO_NUMBER(NVAL(MAX(D.COL9))) AS COL9,
+         TO_NUMBER(NVAL(MAX(D.COL1))) AS COL10,
+         TO_NUMBER(NVAL(MAX(D.COL11))) AS COL11,
+         TO_NUMBER(NVAL(MAX(D.COL12))) AS COL12,
+         TO_NUMBER(NVAL(MAX(D.COL13))) AS COL13,
+         TO_NUMBER(NVAL(MAX(D.COL14)))  AS COL14,
+         TO_NUMBER(NVAL(MAX(D.COL15))) AS COL15,
+         TO_NUMBER(NVAL(MAX(D.COL16))) AS COL16,
+      
+         TO_NUMBER(NVAL(MAX(D.COL17))) AS COL17,
+         TO_NUMBER(NVAL(MAX(D.COL18))) AS COL18,
+         TO_NUMBER(NVAL(MAX(D.COL19))) AS COL19
+       
+        
+        
+FROM
+  VW_DATA_ALL_GC  D
+  
+  
+  
+WHERE
+  (D.PERIOADA IN (:pPERIOADA))  AND   
+  (D.FORM =:pFORM) AND
+  (D.FORM_VERS =:pFORM_VERS) AND 
+  (:pID_MDTABLE =:pID_MDTABLE) AND 
+  (D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%') AND
+  D.FORM IN (58)  AND
+  D.CAPITOL IN (419,416,417,418)
+  
+ 
+  
+  
+  
+  
+  GROUP BY 
+   D.UNIT_CODE,
+   D.CAPITOL,
+   D.RIND
+
+  
+
+
+--HAVING 
+--SUM(D.COL16) <> 0 OR 
+--SUM(D.COL16) < 0
+
+    ORDER BY 
+   D.CAPITOL,
+   D.RIND
