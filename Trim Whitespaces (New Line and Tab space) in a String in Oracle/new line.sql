adb55afa2363d578e554a4be5f4a3a0b8af10573
Vitalie Bancu <VitalieBancu@intern.statistica.md>
2021-04-08 17:33:01 +0300

    initial commit

---
+++ Trim Whitespaces (New Line and Tab space) in a String in Oracle/new line.sql
@@ -0,0 +1,48 @@
+--update CIS2.RENIM
+--
+--SET DENUMIRE = TRIM(translate(DENUMIRE, chr(10)||chr(11)||chr(13), '    ')),
+--
+--    IDNO = TRIM(translate(IDNO, chr(10)||chr(11)||chr(13), '    '))
+
+
+
+
+select 
+CUIIO,
+CUIIO_VERS,
+TRIM(translate(DENUMIRE, chr(10)||chr(11)||chr(13), '    ')) DENUMIRE,
+denumire denumire_wrong,
+TRIM(translate(IDNO, chr(10)||chr(11)||chr(13), '    ')) IDNO,
+IDNO IDNO_wrong
+from CIS2.renim
+
+
+
+where 
+
+(
+
+instr(DENUMIRE, chr(13)) > 0
+OR 
+instr(DENUMIRE, chr(10)) > 0
+OR 
+instr(DENUMIRE, chr(11)) > 0
+
+)
+
+
+
+OR 
+
+(
+instr(IDNO, chr(13)) > 0
+OR 
+instr(IDNO, chr(10)) > 0
+OR 
+instr(IDNO, chr(11)) > 0
+
+)
+
+--AND CUIIO_VERS < 2000
+
+;
