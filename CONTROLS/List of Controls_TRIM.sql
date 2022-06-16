420eefb211185892e2981a9296ea66e4afe332ff
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-06-16 17:49:49 +0300

    List of controls
    List of controls

---
+++ CONTROLS/List of Controls_TRIM.sql
@@ -0,0 +1,56 @@
+﻿SELECT 
+      L.CONTROL,
+      L.FORMULA,
+      L.SQL_TEXT,
+      L.PRIORITATEA,
+      L.STATUT
+      
+      
+        
+        FROM
+
+( 
+SELECT 
+      B.CONTROL,
+      B.FORMULA,
+      B.SQL_TEXT,
+      B.PRIORITATEA,
+      B.STATUT
+      
+      
+        
+        FROM CIS2.MD_CONTROL B  INNER JOIN (
+        SELECT 
+        A.CONTROL, 
+        MAX(A.CONTROL_VERS) CONTROL_VERS
+        
+        FROM CIS2.MD_CONTROL A
+        
+        
+         WHERE
+         1=1
+         AND A.FORM  = 61
+         
+         AND A.CONTROL_VERS <=:pPERIOADA 
+               
+         GROUP BY 
+          A.CONTROL
+         
+         ORDER BY 
+          A.CONTROL
+        
+        )  A  ON A.CONTROL = B.CONTROL AND A.CONTROL_VERS = B.CONTROL_VERS   
+        
+        
+         WHERE
+         1=1
+         AND B.FORM  = 61
+         AND B.STATUT <> '3'
+         
+        
+        
+               
+         
+         
+         ORDER BY 
+          B.CONTROL ) L
